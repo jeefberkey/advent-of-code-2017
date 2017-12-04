5420280821a9500d547ecc834457ad6815fbbc65
Nick Miller <nick.miller@onyxpoint.com>
2017-12-03 21:22:21 -0500

    Day 03: Part a is taking an hour to run

---
+++ day03/example.rb
@@ -0,0 +1,61 @@
+require 'pry'
+
+# https://rosettacode.org/wiki/Ulam_spiral_(for_primes)#Ruby
+# :)
+def cell(n, x, y, start=1)
+  y, x = y - n/2, x - (n - 1)/2
+  l = 2 * [x.abs, y.abs].max
+  d = y >= x ? l*3 + x + y : l - x - y
+  (l - 1)**2 + d + start - 1
+end
+
+def int_to_array(n)
+  format = "%#{(n*n).to_s.size}s "
+  x_max = Math.sqrt(n).ceil
+  y_max = Math.sqrt(n).ceil
+  # spiral = Array.new(x_max) { Array.new(y_max) }
+  spiral = []
+
+  n.times do |y|
+    row = []
+    n.times do |x|
+      i = cell(n,x,y)
+      next if i > n
+      # print format % i
+      row << i
+    end
+    # puts
+    spiral << row
+  end
+  spiral.delete_if { |row| row[0].nil? }
+end
+
+def mat_find(matrix, match)
+  matrix.each_with_index do |row,x|
+    row.each_with_index do |item,y|
+      if matrix[x][y] == match
+        return [x,y]
+      end
+    end
+  end
+end
+
+def manhattan_distance(x1,y1,x2,y2)
+  (x1-x2).abs + (y1-y2).abs
+end
+
+def day03a(input)
+  puts "stop at #{input}"
+
+  spiral = int_to_array(input)
+
+  x1,y1 = mat_find(spiral,1)
+  x2,y2 = mat_find(spiral,input)
+  puts manhattan_distance(x1,y1,x2,y2)
+end
+
+day03a(1)
+day03a(12)
+day03a(23)
+day03a(1024)
+day03a(312051)
