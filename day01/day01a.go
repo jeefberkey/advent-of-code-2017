e5b3ce7ddd55133d7625f84485fecb63aacca1f7
Nick Miller <nick.miller@onyxpoint.com>
2017-12-03 21:24:22 -0500

    am bad at go

---
+++ day01/day01a.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "fmt"
+  "strconv"
+  "strings"
+)
+
+func day01(num int) (out int) {
+  // int to string
+  input := strconv.Itoa(num)
+
+  // string to array
+  digits := strings.Split(input,"")
+
+  
+
+  return
+}
+
+func main() {
+  fmt.Println(day01(1122))
+}
--- day01/day01a.rb
+++ day01/day01a.rb
@@ -5,14 +5,14 @@ INPUT=91212129
 digits = INPUT.to_s.split('')
 
 # easy way
-# matches = []
-# (digits << digits[0]).each_with_index do |d,i|
-#   matches << digits[i].to_i if digits[i] == digits[i+1]
-# end
+matches = []
+(digits << digits[0]).each_with_index do |d,i|
+  matches << digits[i].to_i if digits[i] == digits[i+1]
+end
 
 # hard way
-matches = (digits << digits[0]).map.with_index { |d,i|
-  digits[i].to_i if digits[i] == digits[i+1]
-}.compact
+# matches = (digits << digits[0]).map.with_index { |d,i|
+#   digits[i].to_i if digits[i] == digits[i+1]
+# }.compact
 
 puts matches.sum
