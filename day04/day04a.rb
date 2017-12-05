dc4e1460fd5f74db2e825e532b9aef128b6e7e8d
Nick Miller <nick.miller@onyxpoint.com>
2017-12-04 21:26:37 -0500

    Day04: one line

--- day04/day04a.rb
+++ day04/day04a.rb
@@ -1,17 +1,21 @@
 require 'pry'
 
-def validate_passphrase(phrase)
-  p = phrase.split(' ')
-  p.uniq == p
-end
+# def validate_passphrase(phrase)
+#   p = phrase.split(' ')
+#   p.uniq == p
+# end
+#
+# puts validate_passphrase('aa bb cc dd ee')
+# puts validate_passphrase('aa bb cc dd aa')
+# puts validate_passphrase('aa bb cc dd aaa')
+#
+# valid = 0
+# File.readlines('passphrases.txt').each do |phrase|
+#   valid += 1 if validate_passphrase(phrase)
+# end
+#
+# puts valid
 
-puts validate_passphrase('aa bb cc dd ee')
-puts validate_passphrase('aa bb cc dd aa')
-puts validate_passphrase('aa bb cc dd aaa')
-
-valid = 0
-File.readlines('passphrases.txt').each do |phrase|
-  valid += 1 if validate_passphrase(phrase)
-end
-
-puts valid
+# puts (File.readlines('passphrases.txt').map {|p|p.split(' ').uniq==p.split(' ')}).map {|i|i==true ? 1:0}.sum
+# puts (File.readlines('passphrases.txt').map {|p|p.split(' ').uniq==p.split(' ')}).map {|i|i==true ? 1:0}.sum
+puts (File.readlines('passphrases.txt').map{|p|s=p.split(' ');(s.uniq==s)==true ?1:0}).sum
