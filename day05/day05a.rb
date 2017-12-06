36c0b7461c359eb9caf7d6ad19c6a65f049e87fd
Nick Miller <nick.miller@onyxpoint.com>
2017-12-05 20:53:33 -0500

    Day 05

---
+++ day05/day05a.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+def solve(offsets)
+  index = 0
+  count = 0
+  while offset = offsets[index] do
+    offsets[index] += 1
+    index += offset
+    count += 1
+  end
+  count
+end
+
+puts solve(%w[0 3 0 1 -3].map(&:to_i))
+
+puts solve(File.readlines('input.txt').map(&:to_i))
